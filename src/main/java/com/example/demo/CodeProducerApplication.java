86f31ed82ed920136ef5e99af17c3485c12f7a88
Ay <wenyi.huang@e-vada.com>
2018-01-01 19:18:18 +0800

    content

---
+++ src/main/java/com/example/demo/CodeProducerApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeProducerApplication.class, args);
+	}
+}
